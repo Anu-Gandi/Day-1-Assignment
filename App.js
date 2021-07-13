fb256972efd3bab5ffe472b96632afa7e8084d4b
Anu-Gandi <87362985+Anu-Gandi@users.noreply.github.com>
2021-07-13 14:10:36 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from "react"
+     "./style.css"
+import Button from "./Button"
+
+const Apple=()=>{
+    return(
+        <div>
+            <h1 style={{textAlign : "center"}}>Hello LetsUpgrade!!</h1>
+            <h2>HELLO!! I am Anu from Andhra Pradesh. i am pursuing bachelor degree with specilization CSE in Kakinada Institute Of Engineering And Techonology at kakinada</h2>
+            <Button buttonText="ABOUT"/>
+            <Button buttonText="EDUCATION"/>
+            <Button buttonText="PROJECTS"/>
+            <Button buttonText="SKILLS"/>
+        </div>
+        
+    )
+}
+
+export default Apple
